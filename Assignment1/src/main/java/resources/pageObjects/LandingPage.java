d5361cc8bd18a27b503a10e84b79e88322a50efb
abhinavgoyal13 <abhinavgoyal13@gmail.com>
2020-09-22 23:58:24 +0530

    adding page objects

--- Assignment1/src/main/java/resources/pageObjects/LandingPage.java
+++ Assignment1/src/main/java/resources/pageObjects/LandingPage.java
@@ -1,6 +1,20 @@
 package resources.pageObjects;
 
-public class LandingPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import resources.base.BaseClass;
 
+public class LandingPage extends BaseClass {
+    public WebDriver driver;
 
+    public LandingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By linkText = By.linkText("Sign up!");
+
+    public WebElement clickSignUp() {
+        return driver.findElement(linkText);
+    }
 }
